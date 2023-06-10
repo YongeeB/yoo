68bf5097ba8223140983b6c58450064e798db593
planner <agunbiadebasit5950@gmail.com>
2023-06-10 19:06:36 +0100

    second commit

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -2,6 +2,6 @@
 
 export const index = 1;
 export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.20385358.js","_app/immutable/chunks/index.2b6a9462.js","_app/immutable/chunks/singletons.e8594c02.js"];
+export const imports = ["_app/immutable/nodes/1.f272f910.js","_app/immutable/chunks/index.2b6a9462.js","_app/immutable/chunks/singletons.25410d4d.js"];
 export const stylesheets = [];
 export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -4,7 +4,7 @@ export const manifest = {
 	assets: new Set(["camera.jpg","favicon.png","painting.jpg","tie-dye.jpg"]),
 	mimeTypes: {".jpg":"image/jpeg",".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.c6fcb0f3.js","app":"_app/immutable/entry/app.bf77b68e.js","imports":["_app/immutable/entry/start.c6fcb0f3.js","_app/immutable/chunks/index.2b6a9462.js","_app/immutable/chunks/singletons.e8594c02.js","_app/immutable/entry/app.bf77b68e.js","_app/immutable/chunks/index.2b6a9462.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.18dafc3f.js","app":"_app/immutable/entry/app.327f4abd.js","imports":["_app/immutable/entry/start.18dafc3f.js","_app/immutable/chunks/index.2b6a9462.js","_app/immutable/chunks/singletons.25410d4d.js","_app/immutable/entry/app.327f4abd.js","_app/immutable/chunks/index.2b6a9462.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			() => import('../output/server/nodes/0.js'),
 			() => import('../output/server/nodes/1.js'),
